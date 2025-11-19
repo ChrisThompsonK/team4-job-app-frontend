3c052b73d4469fcd67159787c3c0f96d84cd4831
WesleySloan21 <wesley.sloan@kainos.com>
2025-11-19 16:16:45 +0000

    managed-identity

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,3 +19,54 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+# User Assigned Managed Identity
+resource "azurerm_user_assigned_identity" "frontend_identity" {
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  name                = "${var.app_name}-${var.environment}-identity"
+
+  tags = var.tags
+}
+
+# Data source for Key Vault
+data "azurerm_key_vault" "kv" {
+  name                = "team4-job-app-key-vault"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Data source for Container Registry
+data "azurerm_container_registry" "acr" {
+  name                = "aiacademy25"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Role assignment for ACR pull access
+resource "azurerm_role_assignment" "acr_pull" {
+  scope              = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id       = azurerm_user_assigned_identity.frontend_identity.principal_id
+}
+
+# Role assignment for Key Vault secrets access
+resource "azurerm_role_assignment" "keyvault_secrets" {
+  scope              = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id       = azurerm_user_assigned_identity.frontend_identity.principal_id
+}
+
+# Output the managed identity details for use in deployments
+output "managed_identity_id" {
+  value       = azurerm_user_assigned_identity.frontend_identity.id
+  description = "The ID of the user assigned managed identity"
+}
+
+output "managed_identity_principal_id" {
+  value       = azurerm_user_assigned_identity.frontend_identity.principal_id
+  description = "The principal ID of the user assigned managed identity"
+}
+
+output "managed_identity_client_id" {
+  value       = azurerm_user_assigned_identity.frontend_identity.client_id
+  description = "The client ID of the user assigned managed identity"
+}

03b6ef6f06956b5888c74157f4b0b9c1ee6cea05
WesleySloan21 <wesley.sloan@kainos.com>
2025-11-20 13:56:44 +0000

    adding container app

---
+++ infrastructure/container_app_environment.tf
@@ -0,0 +1,63 @@
+resource "azurerm_container_app_environment" "frontend" {
+  name                       = "cae-${var.app_name}-${var.environment}"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.frontend.id
+}
+
+resource "azurerm_log_analytics_workspace" "frontend" {
+  name                = "law-${var.app_name}-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app" "frontend" {
+  name                         = "ca-${var.app_name}-${var.environment}"
+  container_app_environment_id = azurerm_container_app_environment.frontend.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.job_app_frontend.id]
+  }
+
+  registry {
+    server   = data.azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.job_app_frontend.id
+  }
+
+  template {
+    container {
+      name   = "frontend"
+      image  = "${data.azurerm_container_registry.acr.login_server}/job-app-frontend:latest"
+      cpu    = "0.5"
+      memory = "1Gi"
+
+      env {
+        name  = "API_BASE_URL"
+        value = "https://${azurerm_container_app.backend[0].ingress[0].fqdn}"
+      }
+    }
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 3000
+    transport                  = "auto"
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  tags = var.tags
+}
+
+output "frontend_url" {
+  value = "https://${azurerm_container_app.frontend.ingress[0].fqdn}"
+}

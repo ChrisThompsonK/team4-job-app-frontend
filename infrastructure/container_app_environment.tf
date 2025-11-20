27243ec85f149c8b15cbbfb0366f9bcf0cb3ed70
WesleySloan21 <wesley.sloan@kainos.com>
2025-11-20 15:09:02 +0000

    fix

--- infrastructure/container_app_environment.tf
+++ infrastructure/container_app_environment.tf
@@ -5,18 +5,11 @@ data "azurerm_container_app" "backend" {
 }
 
 resource "azurerm_container_app_environment" "frontend" {
-  name                       = "cae-${var.app_name}-${var.environment}"
-  location                   = azurerm_resource_group.main.location
-  resource_group_name        = "team4-frontend"
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.frontend.id
-}
-
-resource "azurerm_log_analytics_workspace" "frontend" {
-  name                = "law-${var.app_name}-${var.environment}"
+  name                = "cae-${var.app_name}-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = "team4-frontend"
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+
+  tags = var.tags
 }
 
 resource "azurerm_container_app" "frontend" {
